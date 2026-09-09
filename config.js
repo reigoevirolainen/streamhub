6855cc18d9bff62f93ae78aab457f8d117143322
reigoevirolainen <50046915+reigoevirolainen@users.noreply.github.com>
2026-09-09 22:38:21 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.STREAMHUB_CONFIG = {
   SUPABASE_URL: "https://rrzglnazdppgjjtaswmd.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_ax0HpMi18hz-AQ2x8XOT3w_gRLYKE4h"
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_ax0HpMi18hz-AQ2x8XOT3w_gRLYKE4h",
+
 };
